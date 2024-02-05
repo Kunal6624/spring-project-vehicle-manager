4550333ffe76dcbcadcc44571c26fad052a19dc8
kunal <kunallad2466@gmail.com>
2024-02-05 10:45:02 +0530

    add changes

--- src/main/java/com/sipl/vehicle/manager/model/Vehicle.java
+++ src/main/java/com/sipl/vehicle/manager/model/Vehicle.java
@@ -3,6 +3,7 @@ package com.sipl.vehicle.manager.model;
 import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
